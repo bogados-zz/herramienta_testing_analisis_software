294b29cbaa9d797c4844e0d4dc42e98661377830
sbogado <sebastian.bogado@snoopconsultin.com>
2016-04-29 17:17:57 -0300

    Se agregan un par de cosas locas.

--- src/main/java/ar/edu/unlam/analisisSoftware/ehims/utils/Project.java
+++ src/main/java/ar/edu/unlam/analisisSoftware/ehims/utils/Project.java
@@ -1,18 +1,45 @@
 package ar.edu.unlam.analisisSoftware.ehims.utils;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Project {
-	
+	private File carpetaPrincipal;
 	private static Project instance;
-	
+	ArrayList<ClaseJava> clasesJava;
 	private Project(){
 	}
 	
-	public static Project getInstance(){
+	public static Project getInstance(File carpetaPrincipal){
 		if(instance == null){
 			instance = new Project();
 		}
+		
+		instance.setCarpetaPrincipal(carpetaPrincipal);
 		return instance;
 	}
 	
+	private void recorrerCarpetas(File archivoPadre){
+		File[] files = archivoPadre.listFiles();
+		if(files != null && files.length > 0){
+			for(int i=0; i<files.length; i++){
+				recorrerCarpetas(files[i]);
+			}
+		}
+		else{
+			if(archivoPadre.getName().matches(".*java")){
+				clasesJava.add(new ClaseJava(archivoPadre));
+			}
+		}
+		
+	}
+
+	public File getCarpetaPrincipal() {
+		return carpetaPrincipal;
+	}
+
+	public void setCarpetaPrincipal(File carpetaPrincipal) {
+		this.carpetaPrincipal = carpetaPrincipal;
+	}
 	
 }
